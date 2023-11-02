33ae9754268f21da7b630d617be38d0b5c896593
Tstrand123 <tstrandg@uci.edu>
2023-11-02 15:58:08 -0700

    Incremental Update

--- src/RecipeManager/GUI/AddRecipeControls.java
+++ src/RecipeManager/GUI/AddRecipeControls.java
@@ -1,5 +1,7 @@
 package RecipeManager.GUI;
 
+import RecipeManager.logic.TextFieldActionListener;
+
 import javax.swing.*;
 import java.awt.*;
 import java.util.Map;
@@ -7,13 +9,26 @@ import java.util.TreeMap;
 
 public class AddRecipeControls {
     JPanel mainFrame = new JPanel();
+    JPanel mainGrid = new JPanel();
+    JLabel testLabel = new JLabel();
+    JTextField testText = new JTextField();
+    TextFieldActionListener testLogic = new TextFieldActionListener();
 
     public AddRecipeControls(){
         this.mainFrame.setLayout(new BorderLayout());
         this.mainFrame.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+
+        this.mainGrid.setLayout(new GridLayout(2,1));
+        this.mainFrame.add(mainGrid);
+
+        this.testText.addActionListener(testLogic);
+        //System.out.println(testText.getText());
+
+        this.testLabel.setText("Test");
+        this.mainGrid.add(testLabel);
+        this.mainGrid.add(testText);
+
     }
 
-    public JPanel getAddControls(){
-        return this.mainFrame;
-    }
+    public JPanel getAddControls(){ return this.mainFrame; }
 }
---
+++ src/RecipeManager/logic/TextFieldActionListener.java
@@ -0,0 +1,10 @@
+package RecipeManager.logic;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TextFieldActionListener implements ActionListener {
+    public void actionPerformed(ActionEvent e){
+        System.out.println(e.toString());
+    }
+}
