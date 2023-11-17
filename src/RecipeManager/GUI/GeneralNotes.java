06dbc942b30dd4f2728f5f5e39a4b9e058c731e6
Tstrand123 <tstrandg@uci.edu>
2023-11-17 14:55:50 -0800

    New Notes Tab

---
+++ src/RecipeManager/GUI/GeneralNotes.java
@@ -0,0 +1,11 @@
+package RecipeManager.GUI;
+
+import javax.swing.*;
+
+public class GeneralNotes {
+    JPanel mainFrame = new JPanel();
+
+    public JPanel getNotesFrame() {
+        return this.mainFrame;
+    }
+}
